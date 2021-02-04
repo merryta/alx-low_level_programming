c7d74bcd66145079bda61a1cf57f3d5a6d270164
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 17:39:17 +0300

    5-print_numbers.c
    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main () {
+
+   /* local variable definition */
+   int a = 0;
+
+   /* while loop execution */
+   while( a < 10 ) {
+      printf("value of a: %d\n", a);
+      a++;
+   }
+ 
+   return 0;
+}
