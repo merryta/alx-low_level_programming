4a8fc157510c02667a6a2fa861f9a4b5ca24ae30
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 22:45:49 +0300

    10-print_comb2.c
    00 to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
+
+	int i = 48, j = 48;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != 57 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		j = 48;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+© 2021 GitHub, Inc.
