b456ed720353246843c241526dfb52e04e747ee0
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 23:09:19 +0300

    3-print_alphabets.c
    A a to Z z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
-int main () {
-   char ch;
-
-   for(ch = 'A' ; ch <= 'Z' ; ch++) {
-      putchar(ch);
-   }
-   for(ch = 'a' ; ch <= 'z' ; ch++) {
-      putchar(ch);
-   }
-   putchar ('\n');
-   return(0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x = 'a';
+while (x <= 'z')
+{
+putchar(x);
+x++;
+}
+x = 'A';
+while (x <= 'Z')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
 }
