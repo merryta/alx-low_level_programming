a32e185d5568c2037b9f95c5e0b6248502c047cc
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 18:20:18 +0300

    6-print_numberz.c
    print 0-10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h> 
+  
+int main() 
+{ 
+  
+    // Get the character to be written 
+    char ch = '0'; 
+  
+    // Write the Character to stdout 
+    for (ch = '1'; ch <= '10'; ch++) 
+        putchar(ch); 
+  
+    return (0); 
+} 
