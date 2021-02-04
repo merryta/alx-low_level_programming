6e4d5d49924214753ec17046c5fd2de293e1545a
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 23:07:23 +0300

    2-print_alphabet.c
    a to z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+/**
+ * main-prints the alphabet in lowercase, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char alph = 'a';
 
-int main () {
-   char ch;
-
-   for(ch = 'a' ; ch <= 'z' ; ch++) {
-      putchar(ch);
-   }
-   
-    putchar ('\n');
-   
-   return(0);
+	while (alph <= 'z')
+	{
+	putchar (alph);
+	alph++;
+	}
+	putchar ('\n');
+	return (0);
 }
