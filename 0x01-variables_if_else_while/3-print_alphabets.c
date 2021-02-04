edf96f6c8e02a5e559a76e93a8baaa55392a3868
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 16:30:24 +0300

    3-print_alphabets.c
    small and capital letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+   char ch;
+
+   for(ch = 'A' ; ch <= 'Z' ; ch++) {
+      putchar(ch);
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      putchar(ch);
+   
+   return(0);
+}
