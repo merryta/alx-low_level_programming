a54a1c11e11ca9a0e809375b5dabe698be8aa3f6
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 21:58:49 +0300

    3-print_alphabets.c
    a to z and A to Z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,8 +5,10 @@ int main () {
 
    for(ch = 'A' ; ch <= 'Z' ; ch++) {
       putchar(ch);
+   }
    for(ch = 'a' ; ch <= 'z' ; ch++) {
       putchar(ch);
-   
+   }
+   putchar ('\n');
    return(0);
 }
