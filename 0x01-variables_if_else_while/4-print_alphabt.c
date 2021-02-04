3cbff4fe8a3950d4e43e43ad771a56fe154273f3
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 22:09:41 +0300

    4-print_alphabt.c
    No e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
- 
+
 int main () {
-char ch;
-if( ch = e && ch = q  ) {
-  putchar();
-else 
-  putchar(ch);
+   char ch ='a';
+   
+   for(ch = 'a' ; ch <= 'z' ; ch++) {
+      if (ch != 'e' && ch != 'q')
+      putchar(ch);
+   }
+   putchar ('\n');
+   return(0);
 }
