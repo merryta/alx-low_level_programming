bf4944fe2db9755315c78da629d0a4d376044f9b
merryta <77850075+merryta@users.noreply.github.com>
2021-02-04 19:00:34 +0300

    8-print_base16.c
    B16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h> 
+  
+int main() 
+{ 
+  
+    // Get the character to be written 
+    char ch;
+  
+    // Write the Character to stdout 
+    for (ch = '0'; ch <='9'; ch++) 
+        putchar(ch); 
+  
+  for (ch = 'a'; ch <='f'; ch++) 
+        putchar(ch); 
+  
+    return (0); 
+} 
