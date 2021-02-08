83a769f3b270a0ff3f125115e0acbec36a090df0
merryta <77850075+merryta@users.noreply.github.com>
2021-02-08 23:01:12 +0300

    1-alphabet.c
    a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+void print_alphabet(void);
+int i;
+
+for (i='a'; i<='z'; i++) {
+putchar(i);
+}
+
+putchar('/n');
+
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
