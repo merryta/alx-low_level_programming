3972153f8e460059f3ea00e55203feb39f7ee7d3
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 00:38:32 +0300

    5-sign.c
    1 and -1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+int print_sign(int n) {
+
+
+if (n > 0){
+    _putchar('+')
+    return (1);
+} else if (n < 0)
+   _putchar('-')
+    return (-1);
+}else{
+  _putchar('0')
+    return (0);
+}
+return (0);
+)
+
+int main(void)
+{
+    int r;
+
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
