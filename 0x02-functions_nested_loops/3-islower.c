529ca364975cdc880554c1302c07b75374ea740a
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 16:12:24 +0300

    3-islower.c
     lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
 #include "holberton.h"
 
 int _islower(int c) {
- int i = 0;
- int j= 1;
-
-if (c = 'a' && c = 'z') {
-  _putchar(i);
+ 
+ if (c = 'a' && c = 'z') {
+  return (1);
      }else {
-       _putchar(j);
+       return (0);
          {
 
 }
@@ -15,12 +13,8 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+    r = _islower('c');
+     _putchar(r + '0');
+   
     return (0);
 }
