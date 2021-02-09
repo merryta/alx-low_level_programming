ed29132e679c2eeb2b7c31d4488a62b460e830a8
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 18:09:32 +0300

    8-24_hours.c
    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,26 @@
 #include "holberton.h"
 
-void jack_bauer(void){
-int i, j;
-for (i=00, i>= 24, i++){
-  for (j=00, j>=59, j++){
-  
-  _putchar(i / 10 + '0');
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ void jack_bauer(void)
+{
+int i;
+	int j;
+	char del = ':';
+
+	for (i = 00; i <= 23; i++)
+	{
+		for (j = 00 ; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
-			_putchar(':');
+			_putchar(del);
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 			_putchar('\n');
-  }
-}
-
+		}
+	}
 }
