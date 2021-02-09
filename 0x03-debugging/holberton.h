7b1581afba04771e93a5c521a34b3767609d236c
merryta <merryta251@gmail.com>
2021-02-09 16:39:15 +0000

    headr

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+ 
+void poitive_or_negative(int i);
+int Largest_number(int a, int b, int c);
+void print_remaining_ddays(int mont, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* HoLBERTON_H */
