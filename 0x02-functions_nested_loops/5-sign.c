e494ab2d10768b316e519aedd955fa2ff0d47ce1
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 17:25:07 +0300

    5-sign.c
    -,+ and 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,24 +6,17 @@ int print_sign(int n) {
 if (n > 0){
     _putchar('+')
     return (1);
-} else if (n < 0)
+} 
+    else if (n < 0){
    _putchar('-')
     return (-1);
-}else{
+}
+   else if (n= 0){
   _putchar('0')
     return (0);
 }
-return (0);
-)
-
-int main(void)
-{
-    int r;
-
-    r = print_sign(n);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+   return (0);
+  _putchar('\n');
 }
+
+
