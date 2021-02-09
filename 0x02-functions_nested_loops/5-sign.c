c9fe605f067d434b2347c1f99b61ebe2a04722ff
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 17:57:30 +0300

    5-sign.c
    -,+ and 0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,26 @@
 #include "holberton.h"
 
-int print_sign(int n) {
-
-
-if (n > 0){
-    _putchar('+')
-    return (1);
-} 
-    else if (n < 0){
-   _putchar('-')
-    return (-1);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ int print_sign(int n)
+{
+	if (n > 0)
+		{
+		_putchar('+');
+		return (1);
+		}
+	else if (n == 0)
+		{
+		_putchar('0');
+		return (0);
+		}
+	else if (n < 0)
+		{
+		_putchar('-');
+		return (-1);
+		}
+	return (0);
 }
-   else if (n= 0){
-  _putchar('0')
-    return (0);
-}
-   return (0);
-  _putchar('\n');
-}
-
-
