5dedb5e4f1849c77f101874197109689509cd3de
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 17:16:11 +0300

    4-isalpha.c
    checks alphabetic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,13 @@
 #include "holberton.h"
 
-int _isalpha(int c);
+int _isalpha(int c){
 
 if (c = 'a' && c = 'z' || c = 'A' && c = 'Z') {
     return (1);
-}else{
+      }
+    else{
     return (0);
  }
+_putchar('\n');
 }
-int main(void)
-{
-    int r;
 
-    r = _isalpha(c);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
