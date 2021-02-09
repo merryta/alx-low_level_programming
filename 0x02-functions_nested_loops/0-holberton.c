7ea25d6dd56d72fa595bf1800c35cd8436ba610b
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 15:54:52 +0300

    0-holberton.c
    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,20 @@
-gcc -wall-pedantic-werror-wextra_putchar.c 0-holberton.c -o 0-holberton
+#include <stdio.h>
+#include "holberton.h"
+/**
+  * main- protocol holberton school
+  * Return: always zero
+  */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
