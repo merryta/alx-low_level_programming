520af68f222e725e3273bb8464e8d9ef4a1b5997
merryta <77850075+merryta@users.noreply.github.com>
2021-02-09 16:18:53 +0300

    4-isalpha.c
    alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,18 @@
 #include "holberton.h"
 
 int _isalpha(int c);
-int i = 1;
-int j = 0;
+
 if (c = 'a' && c = 'z' || c = 'A' && c = 'Z') {
-    _putchar(i);
+    return (1);
 }else{
-    _putchar(j);
+    return (0);
  }
 }
 int main(void)
 {
     int r;
 
-    r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
+    r = _isalpha(c);
     _putchar(r + '0');
     _putchar('\n');
     return (0);
