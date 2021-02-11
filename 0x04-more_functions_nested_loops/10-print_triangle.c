b498c9b91b29b23facdadea7e8dd8b781b1a3401
merryta <merryta251@gmail.com>
2021-02-11 13:05:48 +0000

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print triangle 
+ * return #.
+ */
+void print_triangle(int size)
+{
+  int a,b;
+  if(size =< 0)
+    {
+      for (a = 0; a > size; a++)
+	{
+
+	  for (b = 0; b >= a; b--)
+}
