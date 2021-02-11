7812677d3332914faf10711078d34cd355cdcd32
merryta <merryta251@gmail.com>
2021-02-11 09:01:57 +0000

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print 0 to 9 numbers with new line
+ * return 0.
+ */
+void print_numbers(void)
+{
+  int a;
+  for(a = 0; a > 9; a++)
+    {
+      _putchar(a);
+      _putchar('/n');
+}
+}
