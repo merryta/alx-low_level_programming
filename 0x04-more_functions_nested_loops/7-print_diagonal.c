ab1b24093b5b309f9766bffe589a3c6236c47654
merryta <merryta251@gmail.com>
2021-02-11 11:07:09 +0000

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *diagonal print
+ *return line
+ */
+void print_diagonal(int n)
+{
+  int a, b;
+
+  if(n <= 0)
+ {
+    for (a =0; a <=n; a++)
+      {
+         for(b = 1; b <= a; b++)
+             {
+             if(b != a)
+             _putchar(' ');
+             else if(b == a)
+               {
+                 _putchar('\\');
+                 _putchar('\n');
+                }
+              }
+       }
+}
+
+  else
+  
+ _putchar('\n');
+}
