0f79be95bde704ddcd749b71fcf2b1b96b66c053
merryta <77850075+merryta@users.noreply.github.com>
2021-02-11 16:20:10 +0300

    0-isupper.c
    check uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,18 @@
-#include "holberton.h"
-/*
- * cheack uppercase character
- * return 1 and 0.
- */
-int _isupper(int c);
-{
-  if(c >= 65 && c <= 90)
-    return(1)
-{
- else{
-   return(0)
-}
 
+#include "holberton.h"
+/**
+  * _isupper - uppercase of a letter
+  * @c: variable of a letter
+  * Return: 1 if upper always 0.
+  */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
