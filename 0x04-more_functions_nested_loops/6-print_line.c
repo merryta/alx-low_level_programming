e86d4def2e7b1ed0f93b13e4dd1de92023daaaeb
merryta <merryta251@gmail.com>
2021-02-11 10:28:46 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * draws a straight line
+ * return int.
+ */
+void print_line(int n)
+{
+  if (n => 0)
+    {
+      _putchar('/n');
+}
+  else{
+    _putchar('_')
+}
+}
