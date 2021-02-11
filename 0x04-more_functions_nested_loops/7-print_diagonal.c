052d13a763b3d0d03414186ec0e481b23dbb90e9
merryta <77850075+merryta@users.noreply.github.com>
2021-02-11 17:14:53 +0300

    7-print_diagonal.c
    print  \\

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,7 +17,7 @@ void print_diagonal(int n)
              _putchar(' ');
              else if(b == a)
                {
-                 _putchar('\\');
+                 _putchar(92);
                  _putchar('\n');
                 }
               }
