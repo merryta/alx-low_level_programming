2c9f881613f70ff8ce98431b802594d92d792496
merryta <77850075+merryta@users.noreply.github.com>
2021-02-11 16:28:54 +0300

    1-isdigit.c
    check 1 to 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,18 @@
+
 #include "holberton.h"
 /**
- * cheacks a digit 0 to 9
- * return 1 and 0
+ * _isdigit - fn for check a number through 0 to 9
+ * @c: is a number
+ * Return: always 0.
  */
 int _isdigit(int c)
 {
-  if (c >= 48 && c <= 57)
-{
-  return(1)
-}
-  else {
-    return(0)
-}
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
