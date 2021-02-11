0c30de9bad9dbba301864daa3609ed34c27acf84
merryta <77850075+merryta@users.noreply.github.com>
2021-02-11 16:54:27 +0300

    4-print_most_numbers
    1-10 !2&4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  */
 void print_most_numbers(void)
 {
-  int b;
-  for(b = 0; b >= 9; b++)
-    {
-      if(b != 2 && b != 4)
-{
-  _putchar(b + '0');
-    
-}
-}
-  _putchar('/n');
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if ((n >= 0 && n < 2) || (n == 3) || (n >= 5 && n <= 9))
+		{
+			_putchar (n + '0');
+		}
+	}
+	_putchar ('\n');
 }
