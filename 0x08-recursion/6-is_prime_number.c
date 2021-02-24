640468205934942f56e2931e40c95165e4c55319
merryta <77850075+merryta@users.noreply.github.com>
2021-02-24 18:45:08 +0300

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,39 +1,31 @@
+  
 #include "holberton.h"
 /**
-* is_prime_number -  function
-* @n: base
-* Return: no zero
-*/
+ *is_prime_check-check if n is prime
+ *@y: integer
+ *@n: integer
+ *Return: prime check or 1 or 0
+ */
+int is_prime_check(int y, int n)
+{
+
+if (n == y)
+return (1);
+if (n % y == 0)
+return (0);
+else
+return (is_prime_check(y + 1, n));
+}
+/**
+ *is_prime_number- find if n is prime
+ *@n: integer
+ *Return: 1 or 0
+ *
+ */
 int is_prime_number(int n)
 {
-	int i = 2;
-	{
-		return (primo(n, i));
-	}
-}
-
-/**
-* primo -  function to find a square
-* @n: number
-* @i: counter
-* Return: no zero
-*/
-int primo(int n, int i)
-{
-	if (i == n)
-	{
-		return (1);
-	}
-	if (n < 2)
-	{
-		return (0);
-	}
-	if (n % i == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (primo(n, i + 1));
-	}
+if (n < 2)
+return (0);
+else
+return (is_prime_check(2, n));
 }
