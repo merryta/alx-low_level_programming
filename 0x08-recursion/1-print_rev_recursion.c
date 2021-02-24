dc07ac02f5619885ff4fd712d0549b94db988382
merryta <77850075+merryta@users.noreply.github.com>
2021-02-24 18:09:48 +0300

    revers print

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,20 @@
 #include "holberton.h"
+
 /**
- * _puts_recursion - prints a string
+ * _print_rev_recursion - prints a string in reverse
  * @s: string coming
- * Return: 0 when string is finished
+ * Return: void
  **/
+
 void _print_rev_recursion(char *s)
 {
-int i = 0;
-
-if (s[i] == '\0')
+if (*(s + 1) == '\0')
 {
 return;
 }
-_putchar (*s);
-s--;
+s++;
 _print_rev_recursion(s);
+_putchar(*s);
+if (*(s - 1) == '\n')
+_putchar (10);
 }
