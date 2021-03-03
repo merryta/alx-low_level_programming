d2fd46b3dd10e2d73ebd9f8869abc895555d804f
merryta <77850075+merryta@users.noreply.github.com>
2021-03-03 03:08:45 +0300

    lower case

--- 0x09-static_libraries/_islower.c
+++ 0x09-static_libraries/_islower.c
@@ -1,1 +1,21 @@
-int _islower(int c);
+#include "holberton.h"
+
+/**
+ * _islower - Check if the entry is in lower.
+ * @c: This is the entry
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
