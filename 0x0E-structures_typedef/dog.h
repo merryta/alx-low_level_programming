38c79982e89304c41e655b014aa661109b546254
merryta <77850075+merryta@users.noreply.github.com>
2021-03-09 13:49:47 +0300

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - new struct type struct dog.
+  * @name: name of dog.
+  * @age: age of dog.
+  * @owner: owner of dog.
+  */
+struct dog
+{
+	char *name;
+	char *owner;
+  float age;
+};
