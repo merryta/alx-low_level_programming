e07efdf4fd9754465cf5e583e6731a4b413001a2
merryta <77850075+merryta@users.noreply.github.com>
2021-03-09 14:02:01 +0300

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
-  * struct dog - new struct type struct dog.
-  * @name: name of dog.
-  * @age: age of dog.
-  * @owner: owner of dog.
+  * struct dog - dog structure
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  *
+  * Description: dog structure with 3 members
+  *
   */
 struct dog
 {
 	char *name;
+	float age;
 	char *owner;
-  float age;
 };
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /* DOG_H */
