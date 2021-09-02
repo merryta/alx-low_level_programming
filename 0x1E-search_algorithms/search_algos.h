d974cf8d7528b7a61e2f1bf1b02398f0e27620c8
merryta <merryta251@gmail.com>
2021-09-02 12:32:15 +0000

    headr

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,27 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
